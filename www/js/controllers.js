f2cd2ff30b71fb937020c0d4c74316b27ebae395
Stefan Gassner <sgas@segas.ch>
2016-03-23 15:26:35 +0100

    Login

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('modal.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, LoginService) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -16,27 +16,67 @@ angular.module('starter.controllers', [])
   $ionicModal.fromTemplateUrl('views/login.html', {
     scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.loginmodal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginmodal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    $scope.loginmodal.show();
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+  $scope.doLogin = function (userLogin) {
+    LoginService.loginUser(userLogin)
+    .then(function (data) {
+        //log in successfull
+        //window.alert("Login funktioniert")
+        $scope.closeLogin();
+        window.location = "#/app/admin";
+    }, function (data) {
+        //log in failed
+    });
   };
+})
+
+.service('LoginService', function ($q, $http) {
+    return {
+        loginUser: function (loginData) {
+            var deferred = $q.defer(),
+                promise = deferred.promise;
+
+            $http({
+                url: 'http://segas.ch/eventino/login.php',
+                method: "POST",
+                data: loginData,
+                headers: { 'Content-Type': 'application/json' }
+            })
+                .then(function (response) {
+                    if (response.data.error.code === "000") {
+                        console.log("User login successful: " + JSON.stringify(response.data));
+                        deferred.resolve(response.data);
+                    } else {
+                        console.log("User login failed: " + JSON.stringify(response.data.error));
+                        deferred.reject(response.data);
+                    }
+                }, function (error) {
+                    console.log("Server Error on login: " + JSON.stringify(error));
+                    deferred.reject(error);
+                });
+
+            promise.success = function (fn) {
+                promise.then(fn);
+                return promise;
+            };
+            promise.error = function (fn) {
+                promise.then(null, fn);
+                return promise;
+            };
+            return promise;
+        },
+    };
 });
